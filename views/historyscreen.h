0b07dcf54882a9a9e0f73d9aa2cd28ef153cf5e2
Bradley Nguyen <137482087+BradleyNgu@users.noreply.github.com>
2025-04-02 18:21:55 -0400

    history

--- views/historyscreen.h
+++ views/historyscreen.h
@@ -30,6 +30,7 @@ public:
     
 signals:
     void backButtonClicked();
+    void homeButtonClicked();
     
 protected:
     void paintEvent(QPaintEvent *event) override;
@@ -62,6 +63,7 @@ private:
     QRadioButton *insulinRadio;
     QRadioButton *combinedRadio;
     QPushButton *backButton;
+    PushButton *homeButton;
     
     void setupUi();
     void connectSignals();
